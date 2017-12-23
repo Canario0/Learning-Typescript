3816b5594ca19209f3fae0a2be61baeb2f5c40f3
Canario0 <prenero4bc@gmail.com>
2017-12-23 22:01:07 +0100

    Addingo some more types test
    we tested boolean, numeric and enum

--- Types/types.js
+++ Types/types.js
@@ -6,4 +6,22 @@ b = 'Rob';
 b = 123;
 //This will throw Type '"Rob"' is not assignable to type 'Number'
 //but it will compile, since it is not an critical error
-//n = 'Rob'; 
+//n = 'Rob';
+//Some boolean test
+var iswinter = false;
+//Error
+//iswinter = 1;
+//Arrays
+var names = ["Jon", "Rickon"];
+//Multitype array
+var names2 = ["Jon", "Rickon", 5];
+//enum
+var Starks;
+(function (Starks) {
+    Starks[Starks["Jon"] = 0] = "Jon";
+    Starks[Starks["Bran"] = 1] = "Bran";
+    Starks[Starks["Eddard"] = 2] = "Eddard";
+    Starks[Starks["Catlyn"] = 3] = "Catlyn";
+})(Starks || (Starks = {}));
+var cat = Starks.Catlyn;
+console.log(cat);
--- Types/types.ts
+++ Types/types.ts
@@ -9,3 +9,25 @@ b = 123;
 //This will throw Type '"Rob"' is not assignable to type 'Number'
 //but it will compile, since it is not an critical error
 //n = 'Rob';
+
+
+
+//Some boolean test
+var iswinter : boolean = false;
+
+//Error
+//iswinter = 1;
+
+//Arrays
+var names : String []= ["Jon", "Rickon"];
+
+//Multitype array
+
+var names2 : any[]= ["Jon", "Rickon", 5];
+
+//enum, their values are Jon=0, Bran=1...
+enum Starks {Jon,Bran,Eddard,Catlyn}
+
+var cat : Starks= Starks.Catlyn;
+
+console.log(cat);
